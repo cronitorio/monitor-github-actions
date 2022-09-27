739708b4d754d266d23db2c2c211e5752fda4ed3
Shane Harter <shane@cronitor.io>
2022-09-26 18:16:28 -0700

    Define action interfaces and implement core sync and telemetry functionality

--- index.js
+++ index.js
@@ -1,21 +1,85 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const cronitor = require('cronitor')(core.getInput('cronitor_key'));
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.info('** Cronitor for Github Actions **');
+    core.setSecret(core.getInput('cronitor_key'))
+    if (!core.getInput('event').workflow) {
+      core.setFailed('Invalid event input: JSON expected')
+      return null
+    }
+    await putMonitorDetails()
+    return sendTelemetry()
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+async function putMonitorDetails() {
+  core.info('Syncing workflow details to Cronitor')
+  const event = core.getInput('event')
+  const cronitor_group = core.getInput('cronitor_group')
+  const cronitor_notify = core.getInput('cronitor_notify')
+
+  const payload = {
+    type: 'job',
+    platform: 'github actions',
+    name: event.workflow.name,
+    key: event.workflow.id,
+    defaultNote: 'Automatically synced using Cronitor for Github Actions. View this workflow on Github: ' +
+      event.workflow.html_url,
+  }
+
+  if (cronitor_group) {
+    payload['group'] = cronitor_group
+  }
+  if (cronitor_notify) {
+    payload['notify'] = [cronitor_notify]
+  }
+
+  return cronitor.Monitor.put(payload)
+}
+
+async function sendTelemetry() {
+  core.info('Sending telemetry to Cronitor');
+  const event = core.getInput('event')
+  const monitor = new cronitor.Monitor(event.workflow.id);
+  const monitorState = getMonitorState()
+
+  if (!monitorState) {
+    core.info('No telemetry to send for this event');
+    return
+  }
+
+  return monitor.ping({
+    state: monitorState,
+    series: event.workflow_run.id,
+    message: getMessage(monitorState),
+  })
+}
+
+function getMonitorState() {
+  const event = core.getInput('event')
+  if (event.action === 'completed' && event.workflow_run.conclusion === 'success') {
+    return 'complete'
+  }
+
+  if (event.action === 'completed' && event.workflow_run.conclusion === 'failure') {
+    return 'fail'
+  }
+
+  if (event.action === 'requested') {
+    return 'run'
+  }
+}
+
+function getMessage(monitorState) {
+  const event = core.getInput('event')
+  if (monitorState === 'run') {
+    return event.workflow_run.html_url
+  }
+}
+
+
+run()
